175e93e5fd67d88eb95295b6e2c189ecab7144d7
PavanOrk <pavanork@gmail.com>
2021-12-02 01:21:06 +0530

    collection usage

--- CollectionsUsage/Program.cs
+++ CollectionsUsage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,35 @@ namespace CollectionsUsage
     {
         static void Main(string[] args)
         {
+            //Arrays
+            int[] Array1 = new int[5];
+            Array1[0] = 1;
+            Console.WriteLine("Array1.Length : " + Array1.Length);
+            Console.WriteLine("Array1.LongLength : " + Array1.LongLength);
+            //ArrayList
+            ArrayList arrList = new ArrayList();
+            for (int i = 0; i < 4; i++)
+            {
+                arrList.Add(1);
+                arrList.Add("Pavan");
+                arrList.Add(true);
+                arrList.Add(3.21m);
+                arrList.Add(new Vendor() { CompanyID = 1, CompanyName = "ABC Corp" });
+                arrList.Add(false);
+                arrList.Add(2);
+                arrList.Add(3.14f);
+                arrList.Add("Kumar");
+            }
+            Console.WriteLine("arrList.Capacity : " + arrList.Capacity);
+            Console.WriteLine("arrList.Count : " + arrList.Count);
+
             Console.WriteLine("Hello World!! Lets Learn.....");
             Console.ReadLine();
         }
+    }
+    class Vendor
+    {
+        public int CompanyID { get; set; }
+        public String CompanyName { get; set; }
     }
 }
